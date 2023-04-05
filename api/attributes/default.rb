e28ba3a5bf48649a814e65ae5d70a1d2cafd7595
Takatoshi Maeda <me@tmd.tw>
2023-04-05 15:32:34 +0900

    Remove virtualenv version fix

--- api/attributes/default.rb
+++ api/attributes/default.rb
@@ -2,4 +2,3 @@ default['celery']['config'] = '/etc/default/celeryd'
 default['app']['revision'] = 'master'
 default['db']['conn_max_age'] = 60
 default['python']['setuptools_version'] = '41.2.0'
-default['python']['virtualenv_version'] = '20.15.1'
